ac75063e2698bcb319195cee0e15f476618b1320
POOMRE <igor.poomre@gmail.com>
2023-08-03 01:07:38 +0300

    Small updates

--- src/test/java/utils/ConfigProperties.java
+++ src/test/java/utils/ConfigProperties.java
@@ -1,36 +1,48 @@
 package utils;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+/**
+ * ConfigProperties is a utility class to read properties from a configuration file.
+ */
 public class ConfigProperties {
 
-    protected static FileInputStream fileInputStream;
-    protected static Properties PROPERTIES;
-    protected static File file;
+    private static FileInputStream fileInputStream;
+    private static Properties properties;
 
     static {
         try {
-            File file1 = new File("src/test/resources/testdata.properties");
-            fileInputStream = new FileInputStream(file1);
-            PROPERTIES = new Properties();
-            PROPERTIES.load(fileInputStream);
+            // Load the configuration file from the "src/test/resources" directory
+            fileInputStream = new FileInputStream("src/test/resources/testdata.properties");
+            properties = new Properties();
+            properties.load(fileInputStream);
         } catch (IOException e) {
+            // Handle the exception if the file is not found or there is an I/O error
             e.printStackTrace();
         } finally {
-            if (fileInputStream != null)
+            // Close the FileInputStream to release resources
+            if (fileInputStream != null) {
                 try {
                     fileInputStream.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+            }
         }
     }
 
+    /**
+     * Get the value of the property corresponding to the given key from the configuration file.
+     *
+     * @param key The key for the property.
+     * @return The value of the property, or null if the key is not found.
+     */
     public static String getTestProperty(String key) {
-        return PROPERTIES.getProperty(key);
+        return properties.getProperty(key);
     }
 }
+
+
 
